e5064fac78d48ed5ec75e49d73229a8c171b3a11
tanmaydsfmklmfksmfsdf <141449907+tanmaydsfmklmfksmfsdf@users.noreply.github.com>
2025-05-22 13:08:02 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,15 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+// Database credentials
+$servername = "localhost";
+$username = "root";       // default XAMPP user
+$password = "";           // default is empty
+$dbname = "dream_drive";
 
-    // Do something (like save to DB, send mail, etc.)
-    echo "Message received successfully!";
-} else {
-    // Block other request methods
-    http_response_code(405);
-    echo "405 Method Not Allowed";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Get form values
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Prepare SQL query
+$sql = "INSERT INTO contact_form (name, email, subject, message) 
+        VALUES ('$name', '$email', '$subject', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Thank you! Your message has been received.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close connection
+$conn->close();
 ?>
+
